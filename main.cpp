8001367a214bbfbac0bac3ba68edb9b2c39600e5
Jack Olney <jack.olney11@imperial.ac.uk>
2014-09-30 19:04:27 +0100

    notes on branches

--- main.cpp
+++ main.cpp
@@ -11,4 +11,15 @@ int main() {
 	return 0;
 }
 
-//branchB edit. is this working?
+//git notes on branches
+////
+//git init
+//git remote add origin 'url to repo on github'
+//git remote -v
+//git push -u origin master #tracks the master on github
+//git checkout -b 'branchname' #creates new branch and switches to it.
+//#remember to reopen the file you were looking at.
+//git add --all
+//git commit -m "message"
+//
+//git push --all -u #This pushes all branches and master to github and tracks upstream.
