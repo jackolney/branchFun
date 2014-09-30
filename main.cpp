aa764fb37db8d30198a8c8572c85de1600fb1cc4
Jack Olney <jack.olney11@imperial.ac.uk>
2014-09-30 18:55:05 +0100

    branch edits

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include "math.h"
 
 using namespace std;
 
@@ -7,8 +6,9 @@ int main() {
 
 	cout << "Hello, Jack!" << endl;
 	cout << "I'm just adding text for fun." << endl;
-
-	cout << "new branch on 'branchB' so this should be somewhere..."
+	cout << "branch fun fun fun" << endl;
 
 	return 0;
 }
+
+//branchB edit. is this working?
