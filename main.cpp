d24d5f2af765135165fc700c9d39933512b182f2
Jack Olney <jack.olney11@imperial.ac.uk>
2014-09-30 19:07:43 +0100

    mergeed, I think

--- main.cpp
+++ main.cpp
@@ -6,8 +6,22 @@ int main() {
 
 	cout << "Hello, Jack!" << endl;
 	cout << "I'm just adding text for fun." << endl;
+	cout << "branch fun fun fun" << endl;
 
 //I AM THE MASTER!
 
 	return 0;
 }
+
+//git notes on branches
+////
+//git init
+//git remote add origin 'url to repo on github'
+//git remote -v
+//git push -u origin master #tracks the master on github
+//git checkout -b 'branchname' #creates new branch and switches to it.
+//#remember to reopen the file you were looking at.
+//git add --all
+//git commit -m "message"
+//
+//git push --all -u #This pushes all branches and master to github and tracks upstream.
