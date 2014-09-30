250ae8882726ad6fda0594d7573d8ca9a9bbe5c3
Jack Olney <jack.olney11@imperial.ac.uk>
2014-09-30 18:52:56 +0100

    edits

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "math.h"
 
 using namespace std;
 
@@ -6,6 +7,8 @@ int main() {
 
 	cout << "Hello, Jack!" << endl;
 	cout << "I'm just adding text for fun." << endl;
+
+	cout << "new branch on 'branchB' so this should be somewhere..."
 
 	return 0;
 }
